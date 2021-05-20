4f9c71dac542a202c586b0b393641f2e3ff5f9fa
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2021-05-20 21:16:33 +0300

    first commit

---
+++ app/src/main/java/com/example/bmdc_events/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.bmdc_events;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    CalendarView calendarView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        textView = findViewById(R.id.text);
+
+
+        calendarView = (CalendarView) findViewById(R.id.calender);
+
+        calendarView.setOnDateChangeListener(myCalendarListener);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, Calendar.getInstance().getActualMinimum(Calendar.HOUR_OF_DAY));
+        long date = calendar.getTime().getTime();
+        calendarView.setMinDate(date);
+    }
+
+    CalendarView.OnDateChangeListener myCalendarListener = (view, year, month, day) -> {
+
+        month = month + 1;
+        String newDate = year+"-"+month+"-"+day;
+       textView.setText(newDate);
+    };
+}
