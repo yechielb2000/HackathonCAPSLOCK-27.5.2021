e182b0d372908c2f965fa1e7c061689785bd3192
artur810 <j3123475@gmail.com>
2021-05-25 11:58:23 +0300

    colors.xml updated

--- app/src/main/java/com/example/bmdc_events/MainActivity.java
+++ app/src/main/java/com/example/bmdc_events/MainActivity.java
@@ -10,33 +10,11 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    CalendarView calendarView;
-    TextView textView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //test 0.3
-
-        textView = findViewById(R.id.text);
-
-
-        calendarView = (CalendarView) findViewById(R.id.calender);
-
-        calendarView.setOnDateChangeListener(myCalendarListener);
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, Calendar.getInstance().getActualMinimum(Calendar.HOUR_OF_DAY));
-        long date = calendar.getTime().getTime();
-        calendarView.setMinDate(date);
     }
 
-    CalendarView.OnDateChangeListener myCalendarListener = (view, year, month, day) -> {
-
-        month = month + 1;
-        String newDate = year+"-"+month+"-"+day;
-       textView.setText(newDate);
-    };
 }
